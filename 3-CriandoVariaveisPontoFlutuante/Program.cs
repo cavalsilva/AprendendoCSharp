c246662bceed6ac5a530ad40e382ba477fc61541
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-12-11 23:13:46 -0200

    Criacao projeto VariaveisPontoFlutuante

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variáveis ponto flutuante");
+
+            double salario = 1250.70;
+            Console.WriteLine(salario);
+
+            int brindes = 15;
+            int pessoas = 2;
+            Console.WriteLine(brindes / pessoas);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
