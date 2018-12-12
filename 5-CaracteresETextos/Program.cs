b37d340a01d33d3744e181852c56cf09a7fbe3b3
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-12-11 23:59:18 -0200

    Criacao projeto CaracteresETextos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = @"Listagem de cursos:
+.NET
+Java
+Javascript";
+            Console.WriteLine(titulo);
+
+            Console.ReadLine();
+        }
+    }
+}
