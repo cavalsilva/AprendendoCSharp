8a004cd294791a5c1729e9b35ed84799d6a83e90
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-12-13 22:52:48 -0200

    Inclusao dos projetos de atribuicao e condicionais

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 29;
+            int idadeRicardo = idade;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeRicardo);
+
+            Console.ReadLine();
+        }
+    }
+}
